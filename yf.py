aab1f541b7a171572ff555fd883e2871807ba055
NULLSM <111129040+NULLSM@users.noreply.github.com>
2022-08-13 10:14:45 +0630

    Update yf.py
    Additional

--- yf.py
+++ yf.py
@@ -1,5 +1,27 @@
 import requests
+import json
+import datetime 
+import random 
+import string 
+import time 
+import os 
+import sys
+import urllib.parse
+ 
 from requests.structures import CaseInsensitiveDict
+
+def genString(stringLength):
+	try:
+		letters = string.ascii_letters + string.digits
+		return ''.join(random.choice(letters) for i in range(stringLength))
+	except Exception as error:
+		print(error)		    
+def digitString(stringLength):
+	try:
+		digit = string.digits
+		return ''.join((random.choice(digit) for i in range(stringLength)))    
+	except Exception as error:
+		print(error)	
 
 url = "https://www.your-freedom.net/api.php"
 
@@ -8,10 +30,31 @@ headers["Content-Length"] = "177"
 headers["Content-Type"] = "application/x-www-form-urlencoded"
 headers["Host"] = "www.your-freedom.net"
 headers["Connection"] = "Keep-Alive"
+username=genString(10)
+password=digitString(16)
 
-data = "cmd=create_account&data=%7B%22username%22%3A%22a_fhvkzerkk%22%2C%22password%22%3A%22uux5qQB46p%22%2C%22email%22%3A%22ssjdhjajaaj%40gmail.com%22%2C%22no_verification%22%3Atrue%7D"
+da = {
+  "cmd":"create_account",
+  "data":{
+    "username":username,
+    "password":password,
+    "email":genString(16)+"@gmail.com",
+    "no_verification":True}
+}
 
+
+
+dat =urllib.parse.urlencode(da)
+d1 = dat.replace('%27','%22')
+d2 = d1.replace('+','')
+
+
+data=d2
 
 resp = requests.post(url, headers=headers, data=data)
 
 print(resp.text)
+print("\nUsername : "+username)
+print("\nPassword : "+password)
+
+
